bbeff468190ae63cf2c6a5cdf2db2e0b6e47eb6f
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2025-04-03 01:53:32 -0300

    Add more MockData

--- app/src/main/java/com/austral/pocketdex/util/MockData.kt
+++ app/src/main/java/com/austral/pocketdex/util/MockData.kt
@@ -20,12 +20,17 @@ object MockData {
             height = 7,
             description = "For some time after its birth, it grows by gaining nourishment from the seed on its back."),
 
+        Pokemon(2, "Ivysaur", listOf(GRASS, POISON), 10, "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs."),
+        Pokemon(3, "Venusaur", listOf(GRASS, POISON), 20, "Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight."),
+
         Pokemon(
             id = 4,
             name = "Charmander",
             type =  listOf(FIRE),
             height = 6,
             description = "From the time it is born, a flame burns at the tip of its tail. Its life would end if the flame were to go out."),
+
+        Pokemon(5, "Charmeleon", listOf(FIRE), 11, "It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws."),
 
         Pokemon(
             id = 6,
@@ -43,6 +48,21 @@ object MockData {
         ),
 
         sylveon,
+
+        Pokemon(8, "Wartortle", listOf(WATER), 10, "It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old."),
+        Pokemon(9, "Blastoise", listOf(WATER), 16, "It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell."),
+
+        Pokemon(10, "Caterpie", listOf(BUG), 3, "For protection, it releases a horrible stench from the antenna on its head to drive away enemies."),
+        Pokemon(11, "Metapod", listOf(BUG), 7, "It is waiting for the moment to evolve. At this stage, it can only harden, so it remains motionless to avoid attack."),
+        Pokemon(12, "Butterfree", listOf(BUG, FLYING), 11, "It loves the honey of flowers and can locate flower patches that have even tiny amounts of pollen."),
+        Pokemon(13, "Weedle", listOf(BUG, POISON), 3, "Its poison stinger is very powerful. Its bright-colored body is intended to warn off its enemies."),
+        Pokemon(14, "Kakuna", listOf(BUG, POISON), 6, "Almost incapable of moving, this Pokémon can only harden its shell to protect itself from predators."),
+        Pokemon(15, "Beedrill", listOf(BUG, POISON), 10, "It has three poisonous stingers on its forelegs and its tail. They are used to jab its enemy repeatedly."),
+        Pokemon(16, "Pidgey", listOf(NORMAL, FLYING), 3, "A common sight in forests and woods. It flaps its wings at ground level to kick up blinding sand."),
+        Pokemon(17, "Pidgeotto", listOf(NORMAL, FLYING), 11, "This Pokémon is full of vitality. It constantly flies around its large territory in search of prey."),
+        Pokemon(18, "Pidgeot", listOf(NORMAL, FLYING), 15, "This Pokémon flies at Mach 2 speed, seeking prey. Its large talons are feared as wicked weapons."),
+        Pokemon(19, "Rattata", listOf(NORMAL), 3, "Will chew on anything with its fangs. If you see one, it’s certain that 40 more live in the area."),
+        Pokemon(20, "Raticate", listOf(NORMAL), 7, "Its hind feet are webbed. They act as flippers, so it can swim in rivers and hunt for prey."),
 
         Pokemon(
             id = 25,
@@ -137,14 +157,6 @@ object MockData {
             type = listOf(ELECTRIC, STEEL),
             height = 4,
             description = "Togedemaru stores electricity in its fur, and discharges it when threatened. Its prickly body is made to repel attacks."
-        ),
-
-        Pokemon(
-            id = 16,
-            name = "Pidgey",
-            type = listOf(NORMAL, FLYING),
-            height = 3,
-            description = "Pidgey is a small, brown bird Pokémon. It uses its keen vision to spot prey from a great distance."
         ),
 
         Pokemon(
