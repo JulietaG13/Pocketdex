a6422727bf4908752c2203c4d9afcfea92352531
JulietaG13 <jgarberoglio@mail.austral.edu.ar>
2025-04-20 01:40:04 -0300

    Add localized names for pokemon types

--- app/src/main/java/com/austral/pocketdex/data/model/PokemonType.kt
+++ app/src/main/java/com/austral/pocketdex/data/model/PokemonType.kt
@@ -1,6 +1,8 @@
 package com.austral.pocketdex.data.model
 
+import android.content.Context
 import androidx.compose.ui.graphics.Color
+import com.austral.pocketdex.R
 import com.austral.pocketdex.ui.theme.TypeBug
 import com.austral.pocketdex.ui.theme.TypeDark
 import com.austral.pocketdex.ui.theme.TypeDragon
@@ -39,6 +41,30 @@ enum class PokemonType(val color: Color) {
     DARK(TypeDark),
     STEEL(TypeSteel),
     FAIRY(TypeFairy);
+
+
+    fun getLocalizedName(context: Context): String {
+        return when (this) {
+            NORMAL -> context.getString(R.string.pokemon_type_normal)
+            FIRE -> context.getString(R.string.pokemon_type_fire)
+            WATER -> context.getString(R.string.pokemon_type_water)
+            ELECTRIC -> context.getString(R.string.pokemon_type_electric)
+            GRASS -> context.getString(R.string.pokemon_type_grass)
+            ICE -> context.getString(R.string.pokemon_type_ice)
+            FIGHTING -> context.getString(R.string.pokemon_type_fighting)
+            POISON -> context.getString(R.string.pokemon_type_poison)
+            GROUND -> context.getString(R.string.pokemon_type_ground)
+            FLYING -> context.getString(R.string.pokemon_type_flying)
+            PSYCHIC -> context.getString(R.string.pokemon_type_psychic)
+            BUG -> context.getString(R.string.pokemon_type_bug)
+            ROCK -> context.getString(R.string.pokemon_type_rock)
+            GHOST -> context.getString(R.string.pokemon_type_ghost)
+            DRAGON -> context.getString(R.string.pokemon_type_dragon)
+            DARK -> context.getString(R.string.pokemon_type_dark)
+            STEEL -> context.getString(R.string.pokemon_type_steel)
+            FAIRY -> context.getString(R.string.pokemon_type_fairy)
+        }
+    }
 
     companion object {
         fun fromString(type: String): PokemonType? {
